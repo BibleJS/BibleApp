a6be5e2bc19f890e942afb7ed93066f71256b5d1
Ionică Bizău <bizauionica@yahoo.com>
2015-01-07 21:42:06 +0200

    Refactored parts of code. Syntax changes.

--- bible.js
+++ bible.js
@@ -1,137 +1,80 @@
 #!/usr/bin/env node
 
-// Help output
-const HELP =
-"bible --help"
-+ "\nusage: bible [options] [reference1, reference2, ...]"
-+ "\n"
-+ "\nCLI client for bible.js NPM module. Read the Holy Bible via command line"
-+ "\n"
-+ "\noptions:"
-+ "\n  --v, --version          print the version"
-+ "\n  --lang, --language      set the Bible language"
-+ "\n  --onlyVerses            prevent showing additional outpt"
-+ "\n  --s, --search           get the verses that match to the string or"
-+ "\n                          regular expression provided"
-+ "\n  --rc, --resultColor     set the result color when searching something"
-+ "\n  --help                  print this output"
-+ "\n"
-+ "\nreferences:"
-+ "\n - References separated by spaces (see example):"
-+ "\n"
-+ "\nexample:"
-+ "\n   bible --lang en 'John 1:1-10' 'Genesis 2:3-7'"
-+ "\n   bible --lang ro --search 'Meroza'"
-+ "\n   bible --lang ro --search '/Meroza/gi'"
-+ "\n"
-+ "\nWhen the module is initialized, the packages listed in configuration file,"
-+ "\nare downloaded and used (~/.bible directory).  The configuration is stored"
-+ "\nin a JSON file, in the home directory: ~/.bible-config.json"
-+ "\n"
-+ "\nIf this doesn't exist, it's created at the first `bible` call."
-+ "\n"
-+ "\nYou can create custom packages, including them there (in  the `versions`"
-+ "\nfield). The additional configuration fields are listed below:"
-+ "\n"
-+ "\n - `language`: a string representing the default language (if this is set,"
-+ "\n               `--lang`  is not needed anymore unless you want to override"
-+ "\n               the language value)"
-+ "\n"
-+ "\n - `resultColor`: a string  representing  the  default  result color  when"
-+ "\n                  searching    something   (if  this  is  set,  `--rc`  or"
-+ "\n                  `--resultColor`  options are not needed anymore unless"
-+ "\n                  you want to override the `resultColor` value)"
-+ "\n"
-+ "\n - `searchLimit`: an integer representing max number of verses that will be"
-+ "\n                  output when searching something"
-+ "\n"
-+ "\n"
-+ "\nDocumentation can be found at https://github.com/BibleJS/BibleApp";
-
-// Constants
-const HOME_DIRECTORY = process.env[
-    process.platform == "win32" ? "USERPROFILE" : "HOME"
-];
-const SAMPLE_CONFIGURATION = {
-    versions: {
-        en: {
-            source: "https://github.com/BibleJS/bible-english"
-          , version: "master"
-          , language: "en"
-        },
-        ro: {
-            source: "https://github.com/BibleJS/bible-romanian"
-          , version: "master"
-          , language: "ro"
-        }
-    }
-  , resultColor: "255, 0, 0"
-  , searchLimit: 10
-  , language: /^ro_/.test(process.env.LC_NAME) ? "ro" : "en"
-  , searchLimit: 10
-};
-
-const CONFIG_FILE_PATH = HOME_DIRECTORY + "/.bible-config.json";
-
 // Dependencies
-var Bible = require("bible.js")
+var BibleJS = require("bible.js")
   , Couleurs = require("couleurs")()
   , Debug = require("bug-killer")
   , RegexParser = require("regex-parser")
-  , Yargs = require("yargs").usage(HELP)
-  , argv = Yargs.argv
-  , language = argv.lang || argv.language
-  , search = argv.s || argv.search
-  , searchResultColor = null
+  , Ul = require("ul")
+  , Yargs = require("yargs")
   , OS = require("os")
   , LeTable = require("le-table")
   , Fs = require("fs")
   , config = null
   ;
 
+// Help output
+const HELP = require("./docs/help")
+    , SAMPLE_CONFIGURATION = require("./docs/sample-conf")
+    , CONFIG_FILE_PATH = Ul.USER_DIR + "/.bible-config.json"
+    , SUBMODULES_DIR = Ul.USER_DIR + "/.bible"
+    ;
+
+// CLI args parser
+Yargs = Yargs.usage(HELP);
+var Argv = Yargs.argv
+  , language = Argv.lang || Argv.language
+  , search = Argv.s || Argv.search
+  , searchResultColor = null
+  ;
+
+// Set log level
 Debug.config.logLevel = 3;
 
-// Read configuration file
+// Read the configuration file
 try {
     config = require(CONFIG_FILE_PATH);
 } catch (e) {
     if (e.code === "MODULE_NOT_FOUND") {
         Debug.log(
-            "No configuration file was found. Initing configuration file."
+            "No configuration file was found. Initing the configuration file."
           , "warn"
         );
         Fs.writeFileSync(CONFIG_FILE_PATH, JSON.stringify(
             SAMPLE_CONFIGURATION, null, 4
         ));
         Debug.log(
-            "Configuration file created successfully at the following location: "
+            "The configuration file created successfully at the following location: "
           + CONFIG_FILE_PATH
           , "warn"
         );
         config = require(CONFIG_FILE_PATH);
     } else {
         Debug.log(
-            "Cannot read configuration file. Reason: " + e.code
+            "Cannot read the configuration file. Reason: " + e.code
           , "warn"
         );
     }
 }
 
-// Show version
-if (argv.v || argv.version) {
-    return console.log("Bible.js v" + require("./package").version);
+// Show the version
+if (Argv.v || Argv.version) {
+    return console.log(
+        "Bible " + require("./package").version
+      + "Bible.JS " + require("./package").dependencies["bible.js"]
+    );
 }
 
 // Show help
-var references = argv._;
-if (argv.help ||  (!language && !references.length && !search)) {
+var references = Argv._;
+if (Argv.help ||  (!language && !references.length && !search)) {
     return console.log(Yargs.help());
 }
 
 // Try to get options from config as well
 language = language || config.language;
 searchResultColor = (
-    argv.rc || argv.resultColor || config.resultColor
+    Argv.rc || Argv.resultColor || config.resultColor
 ).split(/[ ,]+/)
 config.searchLimit = config.searchLimit || 10;
 
@@ -214,7 +157,7 @@ function printOutput (err, verses) {
             }
         }
 
-        if (argv.onlyVerses) {
+        if (Argv.onlyVerses) {
             console.log(cVerse.text);
         } else {
             tbl.addRow([
@@ -233,29 +176,29 @@ function printOutput (err, verses) {
     }
 
     // Output
-    if (!argv.onlyVerses) {
+    if (!Argv.onlyVerses) {
         console.log(tbl.toString());
     }
 }
 
-var bibleDirectory = HOME_DIRECTORY + "/.bible";
-if (!Fs.existsSync(bibleDirectory)) {
+// Check if the ~/.bible dir exists
+if (!Fs.existsSync(SUBMODULES_DIR)) {
     Debug.log("~/.bible directory was not found. Downloading packages. This may take a while.", "info");
 }
 
 // Init submodules
-Bible.init(config, function (err) {
+BibleJS.init(config, function (err) {
 
     if (err) { throw err; }
 
     // Create Bible instance
-    var bibleIns = new Bible({language: language});
+    var bibleIns = new BibleJS({language: language});
 
     // Get the verses
     if (references.length) {
         for (var i = 0; i < references.length; ++i) {
             (function (cR) {
-                if (!argv.onlyVerses) {
+                if (!Argv.onlyVerses) {
                     console.log("Reference: " + cR);
                 }
                 bibleIns.get(cR, printOutput);
@@ -265,7 +208,7 @@ Bible.init(config, function (err) {
 
     // Search verses
     if (search) {
-        if (!argv.onlyVerses) {
+        if (!Argv.onlyVerses) {
             console.log("Results for search: " + search);
         }
         bibleIns.search(search, printOutput);
---
+++ docs/sample-conf.js
@@ -0,0 +1,18 @@
+module.exports = {
+    versions: {
+        en: {
+            source: "https://github.com/BibleJS/bible-english"
+          , version: "master"
+          , language: "en"
+        },
+        ro: {
+            source: "https://github.com/BibleJS/bible-romanian"
+          , version: "master"
+          , language: "ro"
+        }
+    }
+  , resultColor: "255, 0, 0"
+  , searchLimit: 10
+  , language: /^ro_/.test(process.env.LC_NAME) ? "ro" : "en"
+  , searchLimit: 10
+};
